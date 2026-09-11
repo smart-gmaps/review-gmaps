436d080d8d19fb8bb11961db75349358c272d6df
smart-gmaps <jasagmaps@gmail.com>
2026-09-11 15:50:00 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -12,13 +12,13 @@ export async function middleware(req: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return req.cookies.get(name)?.value;
         },
-        set(name, value, options) {
+        set(name: string, value: string, options: CookieOptions) {
           res.cookies.set({ name, value, ...options });
         },
-        remove(name, options) {
+        remove(name: string, options: CookieOptions) {
           res.cookies.set({ name, value: "", ...options });
         },
       },
